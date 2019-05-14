09db056f458b743c7478ffd2de1e1e90a20aa983
yfeng46 <yifeng2019@u.northwestern.edu>
2019-05-14 11:47:39 -0500

    Update download.py
    change the s3 definition

--- src/download.py
+++ src/download.py
@@ -1,5 +1,5 @@
 import boto3
 
-s3 = boto3.client('s3')
+s3 = boto3.resource("s3")
 bucket = s3.Bucket('nw-yifeng')
 bucket.download_file('lyrics.csv','music_df.csv','spotify_track_data.csv','topic_dataset.csv','wiki_hot_100s.csv')
