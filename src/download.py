0528b9372cc7ab5f431e4d1c1ed1dd06a3c74f6e
yfeng46 <yifeng2019@u.northwestern.edu>
2019-05-14 13:32:23 -0500

    Update download.py
    download debug

--- src/download.py
+++ src/download.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
     url = 'https://s3-us-west-2.amazonaws.com/nu-yifeng/'
     parser = argparse.ArgumentParser(description="Download data from S3")
     # add argument
-    parser.add_argument("--file_key", help="Name of the file in S3 that you want to download")
-    parser.add_argument("--output_file_path", help="output path for downloaded file")
+    parser.add_argument("file_key", type=str,help="Name of the file in S3 that you want to download")
+    parser.add_argument("output_file_path", type=str,help="output path for downloaded file")
 
     args = parser.parse_args()
     download_data(url,args.file_key, args.output_file_path)
