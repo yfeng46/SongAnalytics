a7f9080c8be980e677737b4c315fe55c0cd8f2a5
yfeng46 <yifeng2019@u.northwestern.edu>
2019-05-14 11:49:24 -0500

    loadingfunctions
    change the table names in both upload and download

--- src/download.py
+++ src/download.py
@@ -2,4 +2,4 @@ import boto3
 
 s3 = boto3.resource("s3")
 bucket = s3.Bucket('nw-yifeng')
-bucket.download_file('lyrics.csv','music_df.csv','spotify_track_data.csv','topic_dataset.csv','wiki_hot_100s.csv')
+bucket.download_file('lyrics.csv','spotify_track_data.csv','wiki_hot_100s.csv')
--- src/upload.py
+++ src/upload.py
@@ -2,4 +2,4 @@ import boto3
 
 s3 = boto3.client('s3')
 
-s3.upload_file('nw-yifeng','lyrics.csv','music_df.csv','spotify_track_data.csv','topic_dataset.csv','wiki_hot_100s.csv')
+s3.upload_file('nw-yifeng','lyrics.csv','spotify_track_data.csv','wiki_hot_100s.csv')
