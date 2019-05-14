7600a6923c5cf46781e797fa31b883b3009d9f9b
yfeng46 <yifeng2019@u.northwestern.edu>
2019-05-14 11:38:48 -0500

    Update tables.py
    update tracks table

--- src/tables.py
+++ src/tables.py
@@ -41,10 +41,10 @@ class wiki_hot_100s(Base):
         wiki_hot_100s_repr = "<wiki_hot_100s(hot_id='%s', rank='%s', title='%s', artist='%s',year='%s')>"
         return tweet_repr % (self.hot_id, self.rank,self.title,self.artist,self.year)
 
-class tracks(Base):
+class billboard_tracks(Base):
     """ Defines the data model for the table `tweet`. """
 
-    __tablename__ = 'tracks'
+    __tablename__ = 'billboard_tracks'
 
     track_id = Column(String(100),primary_key = True, unique=True,nullable=False)
     year = Column(String(10), unique=False, nullable=False)
@@ -71,7 +71,7 @@ class tracks(Base):
     time_signature = Column(Integer,unique=False,nullable=True)
 
     def __repr__(self):
-        lyrics_repr = "<tracks(track_id='%s', year='%s', artist='%s', explicit='%s',track_name='%s',track_2nd_id='%s',danceability='%s',energy='%s',key=='%s',loudness='%s',mode='%s',speechiness='%s',acousticness='%s',instrumentalness='%s',liveness='%s',valence='%s',tempo='%s',track_type='%s',url='%s',track_herf='%s',analysis_url='%s',duration_ms='%s',time_signature='%s'>"
+        lyrics_repr = "<billboard_tracks(track_id='%s', year='%s', artist='%s', explicit='%s',track_name='%s',track_2nd_id='%s',danceability='%s',energy='%s',key=='%s',loudness='%s',mode='%s',speechiness='%s',acousticness='%s',instrumentalness='%s',liveness='%s',valence='%s',tempo='%s',track_type='%s',url='%s',track_herf='%s',analysis_url='%s',duration_ms='%s',time_signature='%s'>"
         return lyrics_repr % (self.track_id, self.year, self.artist, self.explicit,self.track_name,self.track_2nd_id,self.danceability,self.energy,self.key,self.loudness,self.mode,self.speechiness,self.acousticness,self.instrumentalness,self.liveness,self.valence,self.tempo,self.track_type,self.url,self.track_herf,self.analysis_url,self.duration_ms,self.time_signature)
 
 
