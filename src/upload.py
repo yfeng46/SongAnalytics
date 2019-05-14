30c37909dc5369502cc866faa4c11020cc61ef22
yfeng46 <yifeng2019@u.northwestern.edu>
2019-05-14 11:58:33 -0500

    loadingfunctions
    change the method of download and upload

--- src/upload.py
+++ src/upload.py
@@ -1,5 +1,28 @@
 import boto3
+import argparse
+s3 = boto3.client("s3")
 
-s3 = boto3.client('s3')
+def download_data(args):
+    s3.upload_file(args.input_file_path,args.bucket_name,args.output_file_path)
 
-s3.upload_file('nw-yifeng','lyrics.csv','spotify_track_data.csv','wiki_hot_100s.csv')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload data to S3")
+
+    # add argument
+    parser.add_argument("--input_file_path", help="local path for uploaded file")
+    parser.add_argument("--bucket_name", help="s3 bucket name")
+    parser.add_argument("--output_file_path", help="output path for uploaded file")
+
+    args = parser.parse_args()
+    upload_data(args)
+
+
+
+
+
+
+# s3 = boto3.client('s3')
+
+# s3.upload_file('nw-yifeng','lyrics.csv','spotify_track_data.csv','wiki_hot_100s.csv')
