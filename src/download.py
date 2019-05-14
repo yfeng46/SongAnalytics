1d7b4c65630ba3270d5429c16594ad242496a5e9
yfeng46 <yifeng2019@u.northwestern.edu>
2019-05-14 13:19:22 -0500

    Update download.py
    update download function

--- src/download.py
+++ src/download.py
@@ -17,5 +17,24 @@ import boto3
 
 #     args = parser.parse_args()
 
-s3 = boto3.client("s3")
-s3.download_file("mw-yifeng",'lyrics.csv','spotify_track_data.csv','wiki_hot_100s.csv')
+# s3 = boto3.client("s3")
+# s3.download_file("mw-yifeng",'lyrics.csv','spotify_track_data.csv','wiki_hot_100s.csv')
+
+import pandas as pd
+
+
+def download_data(url,filename,save_path):
+    df = pd.read_csv(url+filename,sep=';')
+    df.to_csv(save_path)
+
+
+
+if __name__ == "__main__":
+    url = 'https://s3-us-west-2.amazonaws.com/nu-yifeng/'
+    parser = argparse.ArgumentParser(description="Download data from S3")
+    # add argument
+    parser.add_argument("--file_key", help="Name of the file in S3 that you want to download")
+    parser.add_argument("--output_file_path", help="output path for downloaded file")
+
+    args = parser.parse_args()
+    download_data(url,args.file_key, args.output_file_path)
