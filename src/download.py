0859bf7d5fca93c8e6e4d179be16fb5b861c0271
yfeng46 <yifeng2019@u.northwestern.edu>
2019-05-21 13:44:37 -0500

    Update download.py
    Update the default argument according to PA review

--- src/download.py
+++ src/download.py
@@ -2,12 +2,26 @@ import argparse
 import boto3
 import pandas as pd
 
+logger = logging.getLogger(__name__)
 
 def download_data(url,filename,save_path):
-	path = url+filename
-	print(path)
-	df = pd.read_csv(path,sep=',')
-	df.to_csv(save_path)
+	df = pd.DataFrame{}
+	if isinstance(filename, list):
+		for file in filename:
+			path = url+filename
+			logger.info("download file from "+path)
+			df = pd.read_csv(path,sep=',')
+			df.to_csv(save_path+file)
+			logger.info("save the file to "+save_path+file)
+	elif isinstance(filename, str):
+		path = url+filename
+		logger.info("download file from "+path)
+		df = pd.read_csv(path,sep=',')
+		df.to_csv(save_path+file)
+		logger.info("save the file to "+save_path+file)
+	else:
+		raise CustomException(filename+" is not a valid file name.")
+
 	return df
 
 
@@ -15,7 +29,7 @@ if __name__ == "__main__":
 	url = 'https://s3-us-west-2.amazonaws.com/nu-yifeng/'
 	parser = argparse.ArgumentParser(description="Download data from S3")
 	# add argument
-	parser.add_argument("file_key", type=str,help="Name of the file in S3 that you want to download")
-	parser.add_argument("output_file_path", type=str,help="output path for downloaded file")
+	parser.add_argument("file_key", default = ["lyrics.csv","spotify_track_data.csv","wiki_hot_100s.csv"],type=str,help="Name of the file in S3 that you want to download")
+	parser.add_argument("output_file_path", type=str,help="output directory for downloaded file")
 	args = parser.parse_args()
 	download_data(url,args.file_key, args.output_file_path)
