607f31e4690bff244d74c06d7e766f94d501dca6
yfeng46 <yifeng2019@u.northwestern.edu>
2019-06-06 11:55:04 -0500

    database debug

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ user = os.environ.get("MYSQL_USER")
 password = os.environ.get("MYSQL_PASSWORD")
 host = os.environ.get("MYSQL_HOST")
 port = os.environ.get("MYSQL_PORT")
-DATABASE_NAME = 'song_analytics'
+DATABASE_NAME = 'msia423'
 SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.format(conn_type=conn_type, user=user, password=password, host=host, port=port, DATABASE_NAME=DATABASE_NAME)
 
 
